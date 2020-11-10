e09f775b00c9f91fa18b0fbe4a0916bb50e0be6b
Crowdbotics <team@crowdbotics.com>
2020-11-10 22:41:18 +0000

    Initial commit.

---
+++ ios/super_league_4000x_22497Tests/super_league_4000x_22497Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface super_league_4000x_22497 : XCTestCase
+
+@end
+
+@implementation super_league_4000x_22497
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
